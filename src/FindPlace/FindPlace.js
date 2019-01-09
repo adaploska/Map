63fb4f5d42999d4e41a0ff38da701c41e4a06e7b
adaploska <adawagrocka@wp.pl>
2019-01-09 14:06:39 +0100

    new project map

---
+++ src/FindPlace/FindPlace.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class FindPlace extends Component {
+ render(){
+   return(
+    <div>wyszukaj miejsce </div>
+  
+   )
+ }
+
+}
---
+++ src/InfoPlace/InfoPlace.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class InfoPlace extends Component {
+ render(){
+   return(
+    <div>informacje o miejscu </div>
+  
+   )
+ }
+
+}
